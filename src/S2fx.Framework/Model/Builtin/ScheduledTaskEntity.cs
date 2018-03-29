259764ffd4b70a21afada76ad80204937be03e6b
Wei "oldrev" Li <liwei@sandwych.com>
2018-03-30 01:36:52 +0800

    Add scheduled tasks

---
+++ src/S2fx.Framework/Model/Builtin/ScheduledTaskEntity.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using S2fx.Model.Annotations;
+
+namespace S2fx.Model.Builtin {
+
+    [Entity(EntityName), DisplayName("Scheduled Task")]
+    public class ScheduledTaskEntity : AbstractAuditedEntity {
+        public const string EntityName = "Core.ScheduledTask";
+
+        [Required, MaxLength(256)]
+        public virtual string Name { get; set; }
+
+        [MaxLength(256)]
+        public virtual string DisplayName { get; set; }
+
+        [Required, MaxLength(256)]
+        public virtual string Cron { get; set; }
+
+        [Required]
+        public virtual bool Actived { get; set; }
+
+        [Required, MaxLength(256)]
+        public virtual string Job { get; set; }
+
+        public virtual string Notes { get; set; }
+
+        [OneToManyProperty(nameof(ScheduledTaskInstanceEntity.Task))]
+        public virtual ICollection<ScheduledTaskInstanceEntity> Instances { get; set; }
+    }
+
+}
---
+++ src/S2fx.Framework/Model/Builtin/ScheduledTaskInstanceEntity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using S2fx.Model.Annotations;
+
+namespace S2fx.Model.Builtin {
+
+    [Entity(EntityName), DisplayName("Scheduled Task Instance")]
+    public class ScheduledTaskInstanceEntity : AbstractAuditedEntity {
+        public const string EntityName = "Core.ScheduledTaskInstance";
+
+        [ManyToOneProperty(ScheduledTaskEntity.EntityName, nameof(ScheduledTaskEntity.Instances)), Required]
+        public virtual ScheduledTaskEntity Task { get; set; }
+
+        public virtual string Arguments { get; set; }
+
+        public virtual DateTime StartedOn { get; set; }
+
+        public virtual DateTime EndedOn { get; set; }
+
+    }
+
+}
