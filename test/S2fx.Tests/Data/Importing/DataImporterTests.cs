76f69d4f28bdecca0235c57600725eb4da77fea0
Wei "oldrev" Li <liwei@sandwych.com>
2019-04-23 00:26:39 +0800

    data importing test framework

---
+++ test/S2fx.Tests/Data/Importing/DataImporterTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace S2fx.Tests.Data.Importing {
+
+    public class DataImporterTests : AbstractDataTests {
+
+        [Fact]
+        public void CanImportXmlData() {
+        }
+
+        [Fact]
+        public void CanImportCsvData() {
+
+        }
+
+    }
+
+}
